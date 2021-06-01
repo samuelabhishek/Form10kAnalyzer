ce3682a5994219a2389ddcaabfe5f11d90d6d689
samuelabhishek <samuelabhi96@gmail.com>
2021-06-02 00:05:17 +0530

    Added Basic WordCloud

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 from Form10k import Form10kAnalyzer, Form10kExtractor, clean_text
 import streamlit as st
 import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -22,16 +24,24 @@ def main():
 
     section_entities_df = get_entites_df(myformanalysis)    
 
+    st.image(get_section_wordcloud(myform, myformanalysis))
+
     st.dataframe(subsection_sentiment_df)
 
     st.subheader("Entities found in section")
 
     st.dataframe(section_entities_df)
 
+
 def get_entites_df(myformanalysis):
     entities_label_df = pd.DataFrame(zip(myformanalysis.entities_text_,myformanalysis.entities_label_), columns=['Entity','Label'])
     entities_label_count_df = pd.DataFrame(entities_label_df.value_counts(), columns=['# of Occurances']).reset_index()
     return entities_label_count_df
+
+def get_section_wordcloud(myform, myformanalysis):
+    wordcloud = WordCloud(width = 1200, height = 600, max_font_size=70, prefer_horizontal = 1, stopwords = myformanalysis.entities_text_ + list(myform.nlp.Defaults.stop_words)).generate_from_text(myform.text_section_)
+    return wordcloud.to_image()
+    
 
 
 
