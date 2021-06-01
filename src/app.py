cff04b3fb560ee72427a730da8d77c06bd91d29d
samuelabhishek <samuelabhi96@gmail.com>
2021-06-01 23:13:35 +0530

    Addig a notebook for demo

--- src/app.py
+++ src/app.py
@@ -20,13 +20,20 @@ def main():
     subsection_sentiment_df = pd.DataFrame(zip(myform.subsections_, myformanalysis.subsections_sentiment_polarity_, myformanalysis.subsections_sentiment_subjectivity_), 
     columns = ['Risk Factor', 'Sentiment - Polarity', 'Sentimeny - Subjectivity'])
 
-    section_entities_df = pd.DataFrame(zip(myformanalysis.entities_text_,myformanalysis.entities_label_), columns=['Entity','Label'])
+    section_entities_df = get_entites_df(myformanalysis)    
 
     st.dataframe(subsection_sentiment_df)
 
+    st.subheader("Entities found in section")
+
     st.dataframe(section_entities_df)
 
-    # st.text(myform.text_section_)
+def get_entites_df(myformanalysis):
+    entities_label_df = pd.DataFrame(zip(myformanalysis.entities_text_,myformanalysis.entities_label_), columns=['Entity','Label'])
+    entities_label_count_df = pd.DataFrame(entities_label_df.value_counts(), columns=['# of Occurances']).reset_index()
+    return entities_label_count_df
+
+
 
 
 if __name__ == '__main__':
